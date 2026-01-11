0754a60e4f4ea30750dc8f08fa51a5bb93c2f6f4
Adam LaCasse <adamlacasse@outlook.com>
2026-01-11 11:29:34 -0500

    deploy config

--- run.py
+++ run.py
@@ -10,4 +10,5 @@ config_name = os.getenv("FLASK_ENV", "development")
 app = create_app(config_name)
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+    port = int(os.getenv("PORT", "5000"))
+    app.run(host="0.0.0.0", port=port)
